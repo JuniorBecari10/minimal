9938add2a62831ead1ee4b876db4570275d739c8
Antônio Carlos <antonioocarlos@proton.me>
2025-04-22 15:09:56 -0300

    feat (minvm): add read_file function

--- minvm/main.c
+++ minvm/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, world!");
+char *read_file(const char *path);
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: minvm <bytecode>");
+    }
+
+    char *file = read_file(argv[1]);
+
+    free(file);
     return 0;
+}
+
+char *read_file(const char *path) {
+    // TODO: read file
+    return NULL;
 }
 
