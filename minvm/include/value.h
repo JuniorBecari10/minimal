44fcf96650ae901201c8b770d5a5091ef11ce05c
Antônio Carlos <antonioocarlos@proton.me>
2025-04-29 16:49:56 -0300

    feat (minvm): add value.h header file

---
+++ minvm/include/value.h
@@ -0,0 +1,26 @@
+#ifndef VALUE_H
+#define VALUE_H
+
+typedef double float64;
+
+typedef enum {
+	VALUE_NUMBER,
+	VALUE_STRING,
+	VALUE_BOOL,
+	VALUE_NIL,
+	VALUE_VOID,
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		float64 number;
+		bool boolean;
+
+	} as;
+} Value;
+
+#define NUMBER_VAL(value) ((Value){VALUE_NUMBER, {.number = value}})
+
+#endif
+
